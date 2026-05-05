d6481f8b9522b6b6d4c46e1b4f095e2d5e47c74b
Muadz-coder <24433@burnside.school.nz>
2026-05-05 13:59:13 +1200

    working somehow

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-import sqlite3 
+import sqlite3
+
+DATABASE = 'book.db'
+
+
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT * FROM book;"
+cursor.execute(sql)
+result = cursor.fetchall()
+print(result)
+db.close()
