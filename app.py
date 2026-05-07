608f39256a0ebf3f32d49b95b758984e79fcac49
Muadz-coder <24433@burnside.school.nz>
2026-05-08 10:21:09 +1200

    Finding NEMO

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import sqlite3
 
 DATABASE = 'book.db'
-
-
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
-sql = "SELECT * FROM book;"
-cursor.execute(sql)
+
+book_title = input("What book are you looking for?\nUser: ").title()
+sql = "SELECT * FROM book WHERE book_name = ?;"
+cursor.execute(sql, (book_title,))
 result = cursor.fetchall()
-print(result)
+
+if result:
+    print(result)
+else:
+    print("Not found")
+
 db.close()
