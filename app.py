351b3248580354f40304e23392f736b132cf3064
Muadz-coder <24433@burnside.school.nz>
2026-05-12 10:14:45 +1200

    my first good work since last python assesment :)

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ DATABASE = 'book.db'
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
 
-book_title = input("What book are you looking for?\nUser: ").title()
-sql = "SELECT * FROM book WHERE book_name = ?;"
-cursor.execute(sql, (book_title,))
-result = cursor.fetchall()
+stuff = input('What would you like to do?\nOption: Insert Book Details, Check Book Details, Check Genre Books\nUser: ').lower()
 
-
-if result:
-    for booj in result:
-        print(book[1])
-else:
-    print('not found')
+if 'check book details' in stuff:
+        book_title = input("What book are you looking for?\nUser: ").title()
+        sql = "SELECT * FROM book WHERE book_name = ?;"
+        cursor.execute(sql, (book_title,))
+        result = cursor.fetchall()
+        if result:
+            for book in result:
+                    print(book)
+        else:
+            print('not found')
 
 db.close()
+
+
+
