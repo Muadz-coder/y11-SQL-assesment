e66ae71a355ba2380f6aff6955bc56e7a5bee42d
Muadz-coder <24433@burnside.school.nz>
2026-05-14 12:35:35 +1200

    IT WORKS YAY

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ DATABASE = 'book.db'
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
 
-stuff = input('What would you like to do?\nOption: Insert Book Details, Check Book Details, Check Genre Books\nUser: ').lower()
+stuff = input('What would you like to do?\nOption: Insert Book Details / Check Book Details\nUser: ').lower()
 
 
-if 'check' and 'details' in stuff:
+if 'check' in stuff:
         book_title = input("What book are you looking for?\nUser: ").title()
         sql = "SELECT * FROM book WHERE book_name = ?;"
         cursor.execute(sql, (book_title,))
@@ -18,15 +18,37 @@ if 'check' and 'details' in stuff:
                     sql = "SELECT * FROM genre WHERE genre_id = ?;"
                     cursor.execute(sql, (genre_id,))
                     result = cursor.fetchall()
-                    for genre_name in result:
+                    for genre in result:
                         author_id = book[4]
                         sql = "SELECT * FROM author WHERE author_id = ?;"
                         cursor.execute(sql, (author_id,))
                         result = cursor.fetchall()
                         for author in result:
-                            print(f'Book ID: {book[0]}  - Book Title:  {book[1]}  - Genre: {genre_name[1]}  - Date: {book[3]}  - Author:  {author[1]}')
+                            print(f'Book ID: {book[0]}  - Book Title:  {book[1]}  - Genre: {genre[1]}  - Date: {book[3]}  - Author:  {author[1]}')
                       
         else:
-            print('not found')
+            print('book not found')
+
+if 'insert' in stuff:
+     inst_book = input('Book title: ')
+     sql = "SELECT * FROM genre;"
+     cursor.execute(sql,)
+     result = cursor.fetchall()
+     genres = [all_genre[1] for all_genre in result]
+     print("Available genres:", ", ".join(genres))
+     while True:
+        inst_genre = input('Genre: ').title()
+        if inst_genre in genres:
+             print('try works')
+             break
+        else:
+             print('IT DOES NOT WORK')
+     
+
+
+
+
+
+
 
 db.close()
